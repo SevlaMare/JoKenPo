43968713575a4c04d92f1f655c1c10d0169e476f
Thiago M <sevilamare@gmail.com>
2019-12-12 16:39:48 -0300

    v1.0
    need implement rounds.

--- script.js
+++ script.js
@@ -1,34 +1,61 @@
-var match = 0, lost = 0, draw = 0, win = 0
-
+var win = 0, lost = 0, draw = 0, match = 0
 var computerSelection = 'rock'
-var playerSelection = window.prompt('Var')
-window.alert(playerSelection)
 
-// toda vez que clica roda a fx de novo
+// selectors to scores board #score
+const selWin = document.querySelector('#win')
+const selLost = document.querySelector('#lost')
+const selDraw = document.querySelector('#draw')
+
+// selector to buttons .btn-x
+const selRock = document.querySelector('#rock')
+const selPaper = document.querySelector('#paper')
+const selScissors = document.querySelector('#scissors')
+
+// call fx on button click
+selRock.addEventListener('click', playRock)
+selPaper.addEventListener('click', playPaper)
+selScissors.addEventListener('click', playScissors)
+
+// function to play match and update the scores
 function check(playerSelection, computerSelection){
+    match += 1
     if (playerSelection == computerSelection) {
-        match += 1
         draw += 1
+        return selDraw.textContent = `Draw: ${draw}`
     } else if (playerSelection == 'rock' && computerSelection == 'paper') {
-        match += 1
         lost += 1
+        return selLost.textContent = `Lost: ${lost}`
     } else if (playerSelection == 'rock' && computerSelection == 'scissors') {
-        match += 1
         win += 1
+        return selWin.textContent = `Win: ${win}`
     } else if (playerSelection == 'paper' && computerSelection == 'rock') {
-        match += 1
         win += 1
+        return selWin.textContent = `Win: ${win}`
     } else if (playerSelection == 'paper' && computerSelection == 'scissors') {
-        match += 1
         lost += 1
+        return selLost.textContent = `Lost: ${lost}`
     } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
-        match += 1
         win += 1
+        return selWin.textContent = `Win: ${win}`
     } else if (playerSelection == 'scissors' && computerSelection == 'rock') {
-        match += 1
         lost += 1
+        return selLost.textContent = `Lost: ${lost}`
     }
     
-    selWin = document.querySelector('#win')
-    return selWin.textContent = `Win: ${win}`
+}
+
+// functions to set play choice and check result
+function playRock() {
+    playerSelection = 'rock'
+    check(playerSelection, computerSelection)
+}
+
+function playPaper() {
+    playerSelection = 'paper'
+    check(playerSelection, computerSelection)
+}
+
+function playScissors() {
+    playerSelection = 'scissors'
+    check(playerSelection, computerSelection)
 }
